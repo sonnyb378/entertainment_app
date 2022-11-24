77724e02e32704a02e9f04b7be69041b75e1c2fc
Sonny Baga <sonnybaga@Sonnys-Mac-mini.lan>
2022-11-24 14:27:46 -0800

    refactor: delete env.test

---
+++ __test__/pages/signin.test.tsx
@@ -0,0 +1,11 @@
+import { render, screen, fireEvent } from '@testing-library/react'
+import '@testing-library/jest-dom'
+import {within} from '@testing-library/dom';
+
+
+
+describe("test sign in page", () => {
+    it("must mock Google login", () => {
+
+    })
+})
