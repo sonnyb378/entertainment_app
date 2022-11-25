82b548c09c8e1ec593056a575990ad66a5f5c00a
Sonny Baga <sonnybaga@Sonnys-Mac-mini.lan>
2022-11-24 22:03:12 -0800

    add test files

---
+++ components/layout/Form/InputField/InputField.test.tsx
@@ -0,0 +1,9 @@
+import { render, screen, fireEvent } from '@testing-library/react'
+import '@testing-library/jest-dom'
+import {within} from '@testing-library/dom';
+
+describe("<InputField />", () => {
+    it("must display the input field", () => {
+        
+    })
+})
---
+++ components/layout/footer/Footer.test.tsx
@@ -0,0 +1,9 @@
+import { render, screen, fireEvent } from '@testing-library/react'
+import '@testing-library/jest-dom'
+import {within} from '@testing-library/dom';
+
+describe("<Footer />", () => {
+    it("must display the footer", () => {
+        
+    })
+})
