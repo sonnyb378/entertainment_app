9690450773f45c4ca29d1b96e3434e6f77c8b3d1
Sonny Baga <sonnybaga@Sonnys-Mac-mini.lan>
2022-11-26 12:37:19 -0800

    test: add Navigation component test

--- components/Layout/Navigation/Navigation.test.tsx
+++ components/Layout/Navigation/Navigation.test.tsx
@@ -2,18 +2,40 @@ import { render, screen, fireEvent } from '@testing-library/react'
 import '@testing-library/jest-dom'
 import {within} from '@testing-library/dom';
 
+import { useAuthState } from "react-firebase-hooks/auth";
+
+import Navigation from "./Navigation";
+
 describe("<Navigation />", () => {
+
+    beforeAll(() => {
+        
+    })
 
     afterAll(() => {
         jest.clearAllMocks();
     })
 
     it("must display navigation", () => {
-        
+        const user = useAuthState as jest.Mock;        
+        user.mockReturnValue({
+            id: "somevalue"
+        });
+        render(<Navigation show={ !!user().id }/>)
+        const nav = screen.getByTestId('navigation_container')
+        expect(nav).toHaveAttribute("class", "container")
     })
 
     it("must not display navigation", () => {
-        
+        const user = useAuthState as jest.Mock;        
+        user.mockReturnValue({
+            id: null
+        });
+
+        render(<Navigation show={ !!user().id }/>)
+        const nav = screen.getByTestId('navigation_container')
+        expect(nav).toHaveAttribute("class", "container_hidden")
+
     })
 
 })
